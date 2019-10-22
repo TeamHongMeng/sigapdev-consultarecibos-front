17e5728a52f5b18a7f46783bb55e58759eb87ea8
milagros <milagros.cotrina@unmsm.edu.pe>
2019-10-22 14:08:58 -0500

    "boton"

---
+++ sigapdev-consultarecibos-front-master/src/Componentes/AppTipoDeCambio.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+//import './App.css';
+import '../App2.css';
+import "react-table/react-table.css";
+import Navegacion from './Navegacion';
+import Tabla2 from './Tabla2';
+import Tabla3 from './Tabla3';
+import PropTypes from 'prop-types';
+import Tabla from './Tabla';
+import Tabla4 from './Tabla4';
+
+
+
+class AppTipoDeCambio extends Component {
+
+  render() {
+
+    return (
+      <div>
+          <Tabla />
+          <hr></hr>
+          
+          <Tabla4 />
+          <div>
+            
+          </div>
+      </div>
+
+    );
+  }
+
+}
+
+
+
+export default AppTipoDeCambio;
+
+
+
+
+
